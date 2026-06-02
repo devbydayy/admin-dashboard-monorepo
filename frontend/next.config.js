96ed53ecb12130cf3a1ca67beaa67b83d8ea8e0d
devbydayy <devbyday91@gmail.com>
2026-06-02 07:06:26 +0330

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,15 +9,6 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
-
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api'}/:path*`,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
