5799264c6c4aab1e6f869833464c955f9e0332e6
devbydayy <devbyday91@gmail.com>
2026-08-22 03:56:59 +0330

    Update redis.js

--- backend/src/cache/redis.js
+++ backend/src/cache/redis.js
@@ -1,11 +1,12 @@
 const Redis = require('ioredis');
 
 const redis = process.env.REDIS_URL
-  ? new Redis(process.env.REDIS_URL, { maxRetriesPerRequest: null })
+  ? new Redis(process.env.REDIS_URL, {
+      maxRetriesPerRequest: null,
+    })
   : new Redis({
       host: process.env.REDIS_HOST || '127.0.0.1',
-      port: Number(process.env.REDIS_PORT) || 6379,
-      password: process.env.REDIS_PASSWORD,
+      port: process.env.REDIS_PORT || 6379,
       maxRetriesPerRequest: null,
     });
 
